45fec2aca99099688fec4f25f76a8a3f93a398e7
mikuk <rlaal567@gmail.com>
2021-09-03 16:47:22 +0900

    Add files via upload

---
+++ EnemyAI.cs
@@ -0,0 +1,309 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+//using System;
+
+public class EnemyAI : Log
+{
+    private Transform Ai_targets;
+    public float nextWayPointDistance = 3f;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+    private Animator Loganim;
+
+    [Header("Projectile")]
+    private int Team_State;
+    private float pos1;
+    private float AttackWait = 0.5f;
+
+    // [Tooltip("Position we want to hit")]
+    // public Vector3 targetPos;
+    
+    [Tooltip("Horizontal seppd, in units/sec")]
+    public float speed = 10;
+    private Vector3 startPos;
+    
+    [Tooltip("How high the arc should be, in units")]
+    public float arcHeight = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gladiatorState = GladiatorState.idle;
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        Loganim = GetComponent<Animator>();
+
+        if (TeamSite_IntValue.RuntimeValue == A_Team)
+        {
+            this.gameObject.tag = "A_Team";
+            this.Team_State = A_Team;
+
+        }
+        else if (TeamSite_IntValue.RuntimeValue == B_Team)
+        {
+            this.gameObject.tag = "B_Team";
+            this.Team_State = B_Team;
+        }
+        moveSpeed = InitmoveSpeed.RuntimeValue;
+        health = maxHealth.RuntimeValue;
+        baseAttack = DamageIntValue.RuntimeValue;
+        Level = Level_IntValue.RuntimeValue;
+        ProjectileSpeed_base = ProjectileSpeed.RuntimeValue;
+        AttackSpeed = WeaponSpeed.RuntimeValue;
+        healthBar.SetMaxHealth(health);
+        Alive_BoolValue.RuntimeValue = true;
+        fireDelaySeconds = AttackSpeed;
+        fireDelay = AttackSpeed;
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+        
+    }
+
+    void UpdatePath()
+    {
+        if (TeamSite_IntValue.RuntimeValue == A_Team)
+        {
+            if (GameObject.FindGameObjectWithTag("B_Team"))
+            {
+                Ai_targets = GameObject.FindGameObjectWithTag("B_Team").GetComponent<Transform>();
+            }
+        }
+        else if (TeamSite_IntValue.RuntimeValue == B_Team)
+        {
+            if (GameObject.FindGameObjectWithTag("A_Team"))
+            {
+                Ai_targets = GameObject.FindGameObjectWithTag("A_Team").GetComponent<Transform>();
+            }
+        }
+
+        if (Ai_targets == null) return;
+        pos1 = Vector3.Distance(Ai_targets.position, transform.position);
+        if (pos1 <= attackRadius)
+        {
+            if (canFire)
+            {
+                //StartCoroutine(AttackCo());
+            
+                Vector3 tempVector = (Ai_targets.transform.position - transform.position).normalized;
+                tempVector = tempVector * (attackRadius / pos1);
+
+                int pro = Random.Range(0, 9);
+
+                if (pro > 3)
+                {
+                    GameObject current = Instantiate(projectile, transform.position, Quaternion.identity);
+                    current.GetComponent<Projectile>().InitSet(Ai_targets.transform.position, this.Team_State, ProjectileSpeed_base, baseAttack);
+                    StartCoroutine(AttackCo());
+                }
+                else if (pro >= 3 && pro < 5)
+                {
+                    Vector3 temp = Ai_targets.position;
+                    temp.y += 7f;
+                    GameObject townt = Instantiate(projectile_townt, temp, Quaternion.identity);
+                    townt.GetComponent<Townt_Projectile>().InitSet(Ai_targets.transform.position, TeamSite_IntValue.RuntimeValue, ProjectileSpeed_base * 1.2f, baseAttack * 3);
+
+                    StartCoroutine(Skill_9_Townt());
+                }
+                else
+                {
+                    GameObject stone = Instantiate(projectile_stone, transform.position, Quaternion.identity);
+                    stone.GetComponent<ParabolicRock>().InitSet(Ai_targets.transform.position, TeamSite_IntValue.RuntimeValue, ProjectileSpeed_base * 1.2f, baseAttack * 2);
+                    StartCoroutine(AttackCo());
+                }
+                canFire = false;
+            }
+        }
+
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, Ai_targets.position, OnPathComplete);
+        }
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log("Enemy AIFixedUpdate()");
+
+        fireDelaySeconds -= Time.deltaTime;
+        if (fireDelaySeconds <= 0)
+        {
+            canFire = true;
+            fireDelaySeconds = fireDelay;
+
+        }
+        if (path == null)
+        {
+            return;
+        }
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        //Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        //changeAnim(direction);
+        //Vector2 force = direction * moveSpeed * Time.deltaTime * 200;
+        //rb.AddForce(force);
+
+
+        Vector3 temp = Vector3.MoveTowards(transform.position, Ai_targets.position, moveSpeed * Time.deltaTime);
+        changeAnim(temp - transform.position);
+        myRigidbody.MovePosition(temp);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if(distance < nextWayPointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        gladiatorState = GladiatorState.attack;
+        Loganim.SetBool("attacking", true);
+        yield return new WaitForSeconds(this.AttackWait);
+
+        gladiatorState = GladiatorState.idle;
+        Loganim.SetBool("attacking", false);
+    }
+    private IEnumerator Skill_9_Townt()
+    {
+        gladiatorState = GladiatorState.attack;
+        Loganim.SetBool("Skill_9_Townt", true);
+        yield return new WaitForSeconds(0.67f);  
+
+        gladiatorState = GladiatorState.idle;
+        Loganim.SetBool("Skill_9_Townt", false);
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        health -= damage;
+        healthBar.SetHealth(health);
+        DamagePopupOpen(damage);
+        // Play hurt animation
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public override void TakeDamage(int damage, int this_team)
+    {
+        Debug.Log("TakeDamage Call?\n");
+        if (this_team == this.Team_State)
+        {
+            if (this.gameObject.activeSelf)
+            {
+                health -= damage;
+                healthBar.SetHealth(health);
+                DamagePopupOpen(damage);
+
+                // Play hurt animation
+                StartCoroutine(TakeKnock());
+
+                if (health <= 0)
+                {
+                    Die();
+                }
+            }
+        }
+    }
+
+    private IEnumerator TakeKnock()
+    {
+        // Play hurt animation
+        if (gladiatorState != GladiatorState.attack)
+        {
+            Loganim.SetBool("hurting", true);
+            gladiatorState = GladiatorState.stagger;
+            yield return new WaitForSeconds(0.1f);
+
+            // Play hurt animation
+            Loganim.SetBool("hurting", false);
+            gladiatorState = GladiatorState.idle;
+        }
+    }
+
+    private void DamagePopupOpen(int damage)
+    {
+        //GameObject hudText = Instantiate(hudDamageText);
+        //hudText.transform.position = hudPos.position;
+        //hudText.GetComponent<DmgLogText>().damage = damage;
+        //Vector3 temp = transform.position;
+        //temp.y += 1f;
+        var go = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+        go.GetComponent<TextMesh>().text = damage.ToString();
+        Debug.Log("Log DamagePopupOpen: " + damage);
+    }
+
+    public override void Die()
+    {
+        Debug.Log("Log Die!");
+        //Die Animation
+        DeathEffect();
+
+        //Disable the enemy
+        //this.gameObject.SetActive(false);
+        //this.enabled = false;
+        Alive_BoolValue.RuntimeValue = false;
+        Destroy(this.gameObject);
+    }
+
+    public override void DeathEffect()
+    {
+        if (deathEffect != null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, deathEffectDelay);
+        }
+    }
+    /*    public override void changeAnim(Vector2 direction)
+        {
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                if (direction.x > 0)
+                {
+                    SetAnimFloat(Vector2.right);
+                }
+                else
+                {
+                    SetAnimFloat(Vector2.left);
+                }
+            }
+            else
+            {
+                if (direction.y > 0)
+                {
+                    SetAnimFloat(Vector2.up);
+                }
+                else
+                {
+                    SetAnimFloat(Vector2.down);
+                }
+            }
+        }*/
+}
---
+++ Townt_Projectile.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Townt_Projectile : Projectile
+{
+    [Tooltip("Position we want to hit")]
+    //public Vector3 targetPos;
+    public GameObject hitEffect;
+    public int team;
+    private Vector3 startPos;
+    private Vector3 targetPos2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        lifetimeSeconds = lifetime;
+        startPos = transform.position; 
+    }
+    public override void InitSet(Vector3 this_target, int this_team, float this_projectileSpeed, int this_Damage)
+    {
+        TeamSite_Projectile = this_team;
+        targetPos = this_target;
+        speed = this_projectileSpeed;
+        baseAttack = this_Damage;
+        this.targetPos2 = this_target;
+        this.targetPos2.y += + 7f;
+
+        transform.position = this.targetPos2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetimeSeconds -= Time.deltaTime;
+
+        if (lifetimeSeconds <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        // 1) Only MoveTowards
+        // transform.position = this.targetPos2;
+        Vector3 nextPos = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        transform.position = nextPos;
+        
+        // 2) MoveTowards + Lerp
+        // transform.position = this.targetPos2;
+        Vector3 nextPos = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        float baseY = Mathf.Lerp(transform.position.y, targetPos.y, 0.1f); //7*0.1 -> 0.7
+        nextPos = new Vecotr3(nextPos.position.x, nextPos.position.y - baseY, transform.position.z);
+        transform.position = nextPos;
+
+        // 3) AddForce
+        myRigidbody.AddForce(new Vector3(0,-1,0), ForceMode.Force);
+
+        // 4) velocity?
+        myRigidbody.velocity = new Vector(0,-1,0) * Time.deltaTime;
+    }
+
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (TeamSite_Projectile == A_Team)
+        {
+            if (other.gameObject.CompareTag("B_Team"))
+            {
+                GameObject efftct = Instantiate(hitEffect, transform.position, Quaternion.identity);
+                Destroy(efftct, 0.2f);
+                Destroy(this.gameObject);
+            }
+
+            Collider2D[] hitOrge = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, Orge_MASK);
+            foreach (Collider2D enemy in hitOrge)
+            {
+                enemy.GetComponent<Orge>().TakeDamage_Bteam(baseAttack, B_Team);
+            }
+
+            Collider2D[] hitLog = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, Log_MASK);
+            foreach (Collider2D enemy in hitLog)
+            {
+                enemy.GetComponent<Log>().TakeDamage(baseAttack, B_Team);
+            }
+        }
+        else if (TeamSite_Projectile == B_Team)
+        {
+            if (other.gameObject.CompareTag("A_Team"))
+            {
+                GameObject efftct = Instantiate(hitEffect, transform.position, Quaternion.identity);
+                Destroy(efftct, 0.2f);
+                Destroy(this.gameObject);
+            }
+
+            Collider2D[] hitLog = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, Log_MASK);
+            foreach (Collider2D enemy in hitLog)
+            {
+                enemy.GetComponent<Log>().TakeDamage(baseAttack, A_Team);
+            }
+
+            Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, Orge_MASK);
+            foreach (Collider2D enemy in hitEnemy)
+            {
+                Debug.Log("Stone Damage: " + baseAttack);
+                enemy.GetComponent<NewGladiator>().TakeDamage_Ateam(baseAttack, A_Team);
+            }
+        }
+    }
+}
